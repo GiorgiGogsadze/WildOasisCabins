ced038aeb6533e33e9144ad2c04a5ee1598bab76
GiorgiGogsadze <gogsadzegeorge42@gmail.com>
2024-08-13 13:20:56 +0400

    small correction

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: `${process.env.BASE_URL}/sitemap.xml`,
+    sitemap: `${process.env.BASE_URL}/sitemap`,
   };
 }
